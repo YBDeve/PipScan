7abc51c797965503debb3986c04a1e6444633ab6
YB <161229697+YBDeve@users.noreply.github.com>
2025-12-23 20:02:25 -0500

    Format setup.py for better readability

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pipscan",  # Name of your package (this is what users will install)
-    version="1",  # Version of your package
-    packages=find_packages(),  # Automatically finds your package's modules
+    name="pipscan",
+    version="1",
+    packages=find_packages(),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -11,9 +11,10 @@ setup(
         "Operating System :: OS Independent",
     ],
     author="YB",  # Author's name
-    description="A Python Library used for network port scanning. Choose between single scanning and multi scanning.",  # Short description
-    long_description=open('README.md').read(),  # Detailed description from README
-    long_description_content_type="text/markdown",  # Type of long description
-    url="https://github.com/YBDeve/PipScan",  # URL of your GitHub repo or website
+    description="A Python Library used for network port scanning. Choose between single scanning and multi scanning.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/YBDeve/PipScan",
     license="MIT",  # License
 )
+
